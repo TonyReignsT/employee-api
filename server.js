55256f1402463cca6bf2113bc0f93e7f84c032d5
TonyReignsT <antonyochieng00@gmail.com>
2026-04-01 05:25:55 +0300

    Created and tested the Schema. (D2)

--- server.js
+++ server.js
@@ -1,11 +1,30 @@
 const app = require("./app");
 const connectDB = require("./config/db");
+const employee = require("./src/models/employeeModel");
 
 const PORT = process.env.PORT || 5000;
 
-// connect DB 
+// testing the model
+// const testEmployee = async () => {
+//   const emp = await employee.create({
+//     firstName: "Reigns",
+//     lastName: "Tony",
+//     email: "reigns@test.com",
+//     department: "Finance",
+//     salary: 45000,
+//     hireDate: "2015-01-04"
+//   });
+
+//   console.log(emp);
+// };
+
+// connect DB
+
 connectDB();
 
+// function call
+// testEmployee();
+
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

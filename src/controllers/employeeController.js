a9e2bcd60762502c840fc541dc3df30365837105
TonyReignsT <antonyochieng00@gmail.com>
2026-04-03 06:46:15 +0300

    created service file and moved logic into it. Also refactored the controller.(D4)

--- src/controllers/employeeController.js
+++ src/controllers/employeeController.js
@@ -1,60 +1,65 @@
-const Employee = require("../models/employeeModel");
-
-// const empController = '';
+// const Employee = require("../models/employeeModel");  // Replaced. Now we are using service to run business logic
+const employeeService = require("../services/employeeService");
 
 // CREATE
 exports.createEmployee = async (req, res) => {
-    try {
-        const employee = await Employee.create(req.body);
-        res.status(201).json(employee)
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+  try {
+    const employee = await employeeService.createEmployee(req.body);
+    res.status(201).json(employee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // GET ALL
 exports.getEmployees = async (req, res) => {
-    try {
-        const employees = await Employee.find();
-        res.json(employees);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+  try {
+    const employees = await employeeService.getEmployees();
+    res.json(employees);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // GET ONE
 exports.getEmployeeById = async (req, res) => {
-    try {
-        const employee = await Employee.findById(req.params.id);
+  try {
+    const employee = await employeeService.getEmployeeById(req.params.id);
 
-        (!employee) ? res.status(404).json({message: "Employee not found"}) : res.json(employee);
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+    !employee
+      ? res.status(404).json({ message: "Employee not found" })
+      : res.json(employee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // UPDATE
 exports.updateEmployee = async (req, res) => {
-    try {
-        const employee = await Employee.findByIdAndUpdate(
-            req.params.id,
-            req.body,
-            {new: true}
-        );
+  try {
+    const employee = await employeeService.updateEmployee(
+      req.params.id,
+      req.body,
+      { new: true },
+    );
 
-        (!employee) ? res.status(404).json({message: "Employee not found"}) : res.json(employee);
-    } catch (error) {
-        res.status(500).json({message: error.message})
-    }
+    !employee
+      ? res.status(404).json({ message: "Employee not found" })
+      : res.json(employee);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
 
 // DELETE
 exports.deleteEmployee = async (req, res) => {
-    try {
-        const employee = await Employee.findByIdAndDelete(req.params.id);
+  try {
+    const employee = await employeeService.deleteEmployee(req.params.id);
 
-        (!employee) ? res.status(404).json({message: "Employee not found"}) : res.json({message: "Employee deleted successfully"});;
-    } catch (error) {
-        res.status(500).json({message: error.message});
-    }
+    !employee
+      ? res.status(404).json({ message: "Employee not found" })
+      : res.json({ message: "Employee deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
 };
---
+++ src/services/employeeService.js
@@ -0,0 +1,36 @@
+const Employee = require("../models/employeeModel");
+
+// CREATE
+const createEmployee = async (data) => {
+    return await Employee.create(data);
+};
+
+// GET ALL
+const getEmployees = async () => {
+    return await Employee.find();
+};
+
+// GET ONE
+const getEmployeeById = async (id) => {
+    return await Employee.findById(id);
+};
+
+// UPDATE
+const updateEmployee = async (id, data) => {
+    return await employeeService.findByIdAndUpdate(id, data, {new: true});
+};
+
+// DELETE
+const deleteEmployee = async (id) => {
+    return await Employee.findByIdAndDelete(id);
+};
+
+
+
+module.exports = {
+    createEmployee,
+    getEmployees,
+    getEmployeeById,
+    updateEmployee,
+    deleteEmployee
+};
